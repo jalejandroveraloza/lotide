1499cdbd8c3a8125a9b28766eee9da8cdcd38456
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2022-11-21 19:23:25 -0800

    assert array Equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = function(array, array2){
+  return JSON.stringify(array) === JSON.stringify(array2);
+}
+
+const assertArrayEqual =  function(actual, expected){
+  
+  if(actual.length !== expected.length){
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`)
+    return;
+  }  
+  for(let i = 0; i < actual.length; i++){
+    if(actual[i] !== expected[i]){
+      console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`)
+    return;
+    }
+  }
+  console.log(`🟢🟢🟢Assertion Passed:  ${actual}  ===  ${expected}`)
+  
+};
+
+assertArrayEqual(eqArrays([1, 2, 3], [1, 2, 5]), true); // => should PASS
