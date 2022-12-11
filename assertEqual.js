e6d229411b1a6521c3504c6fb9d50b9e894eece2
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2022-12-11 12:01:07 -0800

    second push

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,11 @@
 // Function Implementation 
-const asserEqual =  function(actual, expected){
+const assertEqual =  function(actual, expected){
   if(actual === expected){
-    return console.log(`🔴🔴🔴 Assertion Passed:  ${actual}  ===  ${expected}`)
+    return console.log(`🟢🟢🟢 Assertion Passed:  ${actual}  ===  ${expected}`)
   } else {
-    return console.log(`🟢🟢🟢 Assertion Failed: ${actual} !== ${expected}`)
+    return console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`)
   }
 };
 
-//Test code
-
-asserEqual("Lighthouse labs", "Bootcamp");
-asserEqual(1, 1);
+//Export
+module.exports = assertEqual;
