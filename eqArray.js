66afa7e9ad950ffe6bf29578d4a77439750efa05
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2022-12-11 19:37:32 -0800

    refactoring function

--- eqArray.js
+++ eqArray.js
@@ -1,20 +1,7 @@
-const assertEqual =  function(actual, expected){
-  if(actual === expected){
-    return console.log(`🟢🟢🟢Assertion Passed:  ${actual}  ===  ${expected}`)
-  } else {
-    return console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`)
-  }
-  
-};
+const assertEqual = require('./assertEqual');
 
-const eqArrays = function(array, array2){
+const eqArray = function(array, array2){
   return JSON.stringify(array) === JSON.stringify(array2);
 }
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArray;
