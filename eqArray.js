09ed4ebc223bf31b337e97fce49bd5652f575a58
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2022-12-12 08:27:03 -0800

    refactoring function

--- eqArray.js
+++ eqArray.js
@@ -2,6 +2,15 @@ const assertEqual = require('./assertEqual');
 
 const eqArray = function(array, array2){
   return JSON.stringify(array) === JSON.stringify(array2);
+  // if(array.length !== array2.length){
+  //   return false
+  // }  
+  // for(let i = 0; i < array.length; i++){
+  //   if(array[i] !== array2[i]){  
+  //   return false
+  //   }
+  // }
+  // return true
 }
 
 module.exports = eqArray;
