5d79830be014360b25899878c7fd6e05ca50591e
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2022-12-12 21:23:54 -0800

    using mocha and chai for testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,17 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+//const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
-assertEqual(tail([5,7]), 7);
+
+
+describe("#tail", () => {
+  it("returns [7] for [5, 7]", () => {
+    assert.deepEqual(tail([5, 7]), [7]);
+  });
+
+  it("returns [7, 9, 12, 16] for [5, 7, 9, 12, 16]", () => {
+    assert.deepEqual(tail([5, 7, 9, 12, 16]), [7, 9, 12, 16]);
+  });
+
+
+});
