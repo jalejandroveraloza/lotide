5cb686560cf2787d1c4149cabe13d2db9ac6639a
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2022-12-13 22:35:18 -0800

    refactorin module exports, not neccesary to call the values, just the keys

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const tail = require('./tail');
 const middle = require('./middle')
 const assertEqual = require('./assertEqual')
 const eqArray = require('./eqArray')
-const assertArraysEqual = require('./assertArraysEqual')
+const assertArraysEqual = require('./assertArraysEqual');
+
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  assertEqual: assertEqual,
-  eqArray: eqArray,
-  assertArraysEqual: assertArraysEqual,
+  head,
+  tail,
+  middle,
+  assertEqual,
+  eqArray,
+  assertArraysEqual
 };
