a7c2ab7a49f49743070212b90db719a926515f02
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2023-01-22 16:01:51 -0800

    fixing errors

--- findKey.js
+++ findKey.js
@@ -1,15 +1,10 @@
-const findkey = function (object1, callback){
-for(let key in object1){
-  for(let key2 in object1[key]){
-    //console.log(object1[key][key2]);
-  
-  //console.log(object1[key]['stars'])
-if(object1[key][key2] === callback){
-  return key;
-}
-}
-}
-}
+const findkey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
 
 const object = {
   "Blue Hill": { stars: 1 },
@@ -18,7 +13,7 @@ const object = {
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}
+};
 
 const results1 = findkey(object, x => x.stars === 2);
 
